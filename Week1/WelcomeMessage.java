1699223e3dbfacdfbd2271105fabe444269f4acf
10pranjal2005 <pk5518@srmist.edu.in>
2025-08-20 21:18:15 +0530

    Add files via upload

---
+++ Week1/WelcomeMessage.java
@@ -0,0 +1,9 @@
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        String name = "Pranjal"; // Replace with your name
+        String s = "Welcome '" + name + "'";
+		String s1 = "Welcome";
+		System.out.println(s1);
+        System.out.println(s);
+	}
+}
